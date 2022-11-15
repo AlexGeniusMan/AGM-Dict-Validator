54df242776a247da089249fae981f00ac76d498f
Alexander Chentsov <alexgeniusman@gmail.com>
2022-11-15 21:02:15 +0400

    upload to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-setup(name='dict_validator',
+setup(name='agm_dict_validator',
       version='0.0.1',
       description='Custom dict validator',
-      packages=['dict_validator'],
+      packages=['agm_dict_validator'],
       author_email='alexgeniusman@gmail.com',
       zip_safe=False)
